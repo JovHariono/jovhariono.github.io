0ae4bd7fcae296e11b554f7a6a4819150e0c251f
JovHariono <128022304+JovHariono@users.noreply.github.com>
2023-10-03 11:28:53 +0700

    update our brand

--- src/pages/ourbrand.tsx
+++ src/pages/ourbrand.tsx
@@ -8,7 +8,6 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import Mudkip2 from "../../public/assets/mudkip2.jpg";
 import Brand1 from "../../public/assets/Brand1(2).png";
 import Brand2 from "../../public/assets/Brand2(2).png";
 import Brand3 from "../../public/assets/Brand3(2).png";
@@ -21,12 +20,51 @@ import sod3 from "../../public/assets/sod3.jpeg";
 import sod4 from "../../public/assets/sod4.jpeg";
 import sod5 from "../../public/assets/sod5.jpeg";
 import sod6 from "../../public/assets/sod6.jpeg";
+import sod7 from "../../public/assets/sod7.jpeg";
+import sod8 from "../../public/assets/sod8.jpeg";
+import sod9 from "../../public/assets/sod9.jpeg";
+import sod10 from "../../public/assets/sod10.jpeg";
+import sod11 from "../../public/assets/sod11.jpeg";
 import waku from "../../public/assets/waku.jpeg";
 import waku2 from "../../public/assets/waku2.jpeg";
 import waku3 from "../../public/assets/waku3.jpeg";
 import waku4 from "../../public/assets/waku4.png";
+import waku5 from "../../public/assets/waku5.jpeg";
+import waku6 from "../../public/assets/waku6.jpeg";
+import waku7 from "../../public/assets/waku7.jpeg";
+import waku8 from "../../public/assets/waku8.jpeg";
+import waku9 from "../../public/assets/waku9.jpeg";
+import waku10 from "../../public/assets/waku10.jpeg";
 import mls1 from "../../public/assets/mls1.png";
 import mls2 from "../../public/assets/mls2.png";
+import cla_ap1 from "../../public/assets/cla-ap1.jpeg";
+import cla_ap2 from "../../public/assets/cla-ap2.jpeg";
+import cla_ap3 from "../../public/assets/cla-ap3.jpeg";
+import cla_i1 from "../../public/assets/cla-i1.jpeg";
+import cla_i2 from "../../public/assets/cla-i2.jpeg";
+import cla_i3 from "../../public/assets/cla-i3.jpeg";
+import cla_b1 from "../../public/assets/cla-b1.jpeg";
+import cla_b2 from "../../public/assets/cla-b2.jpeg";
+import cla_b3 from "../../public/assets/cla-b3.jpeg";
+import cla_b4 from "../../public/assets/cla-b4.jpeg";
+import cla_b5 from "../../public/assets/cla-b5.jpeg";
+import cla_b6 from "../../public/assets/cla-b6.jpeg";
+import sodLive_Rai1 from "../../public/assets/Rai1.jpeg";
+import sodLive_Rai2 from "../../public/assets/Rai2.jpeg";
+import sodLive_Rai3 from "../../public/assets/Rai3.jpeg";
+import sodLive_Rai4 from "../../public/assets/Rai4.jpeg";
+import sodLive_Rai5 from "../../public/assets/Rai5.jpeg";
+import otg1 from "../../public/assets/otg1.jpg";
+import otg2 from "../../public/assets/otg2.jpg";
+import otg3 from "../../public/assets/otg3.jpg";
+import otg4 from "../../public/assets/otg4.jpg";
+import otg5 from "../../public/assets/otg5.jpg";
+import otg6 from "../../public/assets/otg6.jpg";
+import otg7 from "../../public/assets/otg7.jpg";
+import otg8 from "../../public/assets/otg8.jpg";
+import otg9 from "../../public/assets/otg9.jpg";
+import otg10 from "../../public/assets/otg10.jpg";
+
 
 import Footer from "./components/cards/productCards/footer/Footer";
 import { BrandType } from "../type";
@@ -55,7 +93,7 @@ const OurBrand: React.FunctionComponent<IOurBrandProps> = (props) => {
     {
       image: Brand1,
       judul: "SOD Festival",
-      imagePopup: [Brand1, sod1, sod2, sod3, sod4, sod5, sod6],
+      imagePopup: [Brand1, sod1, sod2, sod3, sod4, sod5, sod6, sod7, sod8, sod9, sod10, sod11],
       judulPopUp: "SOD Festival",
       deskripsiPopUp:
         "Events that are organized by PT. Serikat Orang Dalam (SOD), combining music, arts, foods and fashion in one place.",
@@ -67,7 +105,7 @@ const OurBrand: React.FunctionComponent<IOurBrandProps> = (props) => {
     {
       image: Brand2,
       judul: "Chakra Live Asia",
-      imagePopup: [Brand2],
+      imagePopup: [Brand2, cla_ap1, cla_ap2, cla_ap3, cla_i1, cla_i2, cla_i3, cla_b1, cla_b2, cla_b3, cla_b4, cla_b5, cla_b6],
       judulPopUp: "Chakra Live Asia",
       deskripsiPopUp:
         "International music promoter in South East Asia, Indonesia, bringing the best talents and performers across the globe.",
@@ -79,7 +117,7 @@ const OurBrand: React.FunctionComponent<IOurBrandProps> = (props) => {
     {
       image: Brand3,
       judul: "Waku-Waku Festival",
-      imagePopup: [Brand3, waku, waku2, waku3, waku4],
+      imagePopup: [Brand3, waku, waku2, waku3, waku4, waku5, waku6, waku7, waku8, waku9, waku10],
       judulPopUp: "Waku-Waku Festival",
       deskripsiPopUp:
         "Events that consists of Japanese Themed Music, Arts, Foods and Culture Festival. Aiming to create a full experience of japanese culture in one place.",
@@ -91,7 +129,7 @@ const OurBrand: React.FunctionComponent<IOurBrandProps> = (props) => {
     {
       image: Brand5,
       judul: "On The Ground",
-      imagePopup: [Brand5],
+      imagePopup: [Brand5, otg1, otg2, otg3, otg4, otg5, otg6, otg7, otg8, otg9, otg10],
       judulPopUp: "SOD Festival",
       deskripsiPopUp:
         "A company that focuses on helping to provide and create the best experience for audience through our first-class event handling services.",
@@ -103,7 +141,7 @@ const OurBrand: React.FunctionComponent<IOurBrandProps> = (props) => {
     {
       image: Brand6,
       judul: "SOD Live",
-      imagePopup: [Brand6],
+      imagePopup: [Brand6, sodLive_Rai1, sodLive_Rai2, sodLive_Rai3,sodLive_Rai4, sodLive_Rai5],
       judulPopUp: "SOD Live",
       deskripsiPopUp:
         "SOD LIVE is here to bridge local communities to musicians to brands through music collective events.",
